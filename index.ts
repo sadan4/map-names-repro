d8789b961058c1c4dc6e214b8ed3daf85feda1ca
sadan <117494111+sadan4@users.noreply.github.com>
2025-04-10 23:14:40 -0400

    upload

---
+++ index.ts
@@ -0,0 +1,12 @@
+{
+    const nonMinifiedA = Math.random();
+    const variableB = Math.random();
+    if (nonMinifiedA > variableB) {
+        console.log(nonMinifiedA);
+    } else {
+        console.log(variableB);
+    }
+    if (!Math.random()) {
+        require("typescript");
+    }
+}
